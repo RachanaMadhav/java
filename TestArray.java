409d801af97ceac07717279be0c40050e168cce2
RachanaKaparthi <47020625+RachanaMadhav@users.noreply.github.com>
2019-12-27 02:20:56 -0800

    Add files via upload

---
+++ TestArray.java
@@ -0,0 +1,16 @@
+package abcd;
+
+public class TestArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x[]= new int[5];
+		int y[]={12,23,34,45,16};
+		for(int i=0;i<5;i++)
+			x[i]=i+1;
+		for(int i=0;i<5;i++)
+		System.out.println(x[i]+" "+y[i]);
+
+	}
+
+}
